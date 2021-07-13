ba2597603114a3ea178cbc126ed034e0132b45d8
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-07-13 08:00:26 +0530

    Added: two counter elements and their respective store

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@
 import './App.css';
 import StoreOne  from './Stores/storeOne';
 import Mobx from './Components/mobx'
+import CounterMakeObservable from '../src/Stores/makeObservable_Store'
+import CountMakeObservable from '../src/Components/makeObservableCount'
+import CountWithAutoObservabel from '../src/Components/makeAutoObservableCount'
+import MakeAutoObservableStore from '../src/Stores/makeAutoObservable_Store'
+import { makeAutoObservable } from 'mobx';
 
 
 function App() {
-  const store = new StoreOne()
+  // const store = new StoreOne()
+  // const makeObservableStore = new CounterMakeObservable()
+  const makeAutoObservableStore = new MakeAutoObservableStore()
 
   return (
-   <Mobx store = {store} />
+  //  <Mobx store = {store} />
+  // <CountMakeObservable  store = {makeObservableStore} />
+ < CountWithAutoObservabel  store = {makeAutoObservableStore} />
   );
 }
 
