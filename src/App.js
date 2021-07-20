b7669ddfbf3d56d2dfda601811873363d6ef8aec
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-07-20 12:25:50 +0530

    Refactotred: mounted all required example compoenents

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import CounterMakeObservable from '../src/Stores/makeObservable_Store'
 import CountMakeObservable from '../src/Components/makeObservableCount'
 import CountWithAutoObservabel from '../src/Components/makeAutoObservableCount'
 import MakeAutoObservableStore from '../src/Stores/makeAutoObservable_Store'
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, when } from 'mobx';
 // import ActionStore from './Stores/Actions_Store';
 // import Assests from '../src/Components/Actions'
 import ComputedStore from '../src/Stores/Computed_Store'
 import Loan from '../src/Components/Computed'
 import Reaction from '../src/Components/Reactions'
 import ReactionStore from '../src/Stores/Reactions_Store'
+import WhenReaction from './Components/When_Reaction';
+import WhenReactionStore from './Stores/When_Store'
+import LocalObservable from '../src/Components/LocalObservable'
+import StockStore from '../src/StoreMangement/StockStore'
+import Stock from '../src/StoreMangement/stock'
+import stock from '../src/StoreMangement/stock';
 
 function App() {
   // const store = new StoreOne()
@@ -20,14 +26,19 @@ function App() {
   // const makeAutoObservableStore = new MakeAutoObservableStore()
   // const actionStore = new ActionStore()
   // const compStore = new ComputedStore()
-  const reactionStore = new ReactionStore()
+  // const reactionStore = new ReactionStore()
+  // const whenStore = new WhenReactionStore()
+  const stockstore = new StockStore()
   return (
     //  <Mobx store = {store} />
     // <CountMakeObservable  store = {makeObservableStore} />
     //  < CountWithAutoObservabel  store = {makeAutoObservableStore} />
     // <Assests store={actionStore} />
     // <Loan  store = {compStore}/>
-    <Reaction store = {reactionStore} />
+    // <Reaction store = {reactionStore} />
+    // <WhenReaction  store = {whenStore} />
+    // <LocalObservable />
+    <Stock store = {stockstore} />
   );
 }
 
