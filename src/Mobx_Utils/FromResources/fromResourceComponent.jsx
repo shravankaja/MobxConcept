0de5ba9edbb2368096bf0bdd5d6c79353a39407d
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-07-23 11:15:52 +0530

    Added: ui component to use returned fromResource object from store

---
+++ src/Mobx_Utils/FromResources/fromResourceComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FromResourceComponent({fromResource}) {
+    console.log(fromResource)
+    return (
+        <>
+        <div>Hello</div>
+        </>
+    )
+}
+export default FromResourceComponent
